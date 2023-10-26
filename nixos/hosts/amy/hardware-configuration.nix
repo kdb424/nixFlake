d7569d993eee85694d4a518cfe7d74ad2c89bea7
Kyle Brown <kdb424@gmail.com>
2023-10-25 19:05:44 -0700

    feat: Swap and zram

--- nixos/hosts/amy/hardware-configuration.nix
+++ nixos/hosts/amy/hardware-configuration.nix
@@ -26,7 +26,17 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 8*1024;
+    randomEncryption.enable = true; 
+  } ];
+
+  zramSwap = {
+    enable = true;
+    memoryPercent = 40;
+    priority = 10;
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
