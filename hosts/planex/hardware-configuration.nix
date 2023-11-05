7333f7361e1779652d8ef38f4d44a5202d01fb9e
Kyle Brown <kdb424@gmail.com>
2023-11-05 09:20:22 -0800

    fix: Ryzen 1700 lockup issue patch and cleanup

--- hosts/planex/hardware-configuration.nix
+++ hosts/planex/hardware-configuration.nix
@@ -1,46 +1,33 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
 {
-  config,
-  lib,
-  pkgs,
-  modulesPath,
-  ...
-}: {
-  imports = [
-    (modulesPath + "/installer/scan/not-detected.nix")
-  ];
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-amd"];
-  boot.extraModulePackages = [];
-  boot.kernelParams = [ "rcu_nocbs=0-14 processor.max_cstate=1" ]; # Lockup workaround
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  #boot.kernelParams = ["rcu_nocbs=0-15" "idle=nomwait" "pci=nomsi" "processor.max_cstate=1"]; # Ryzen lockup patches
+  boot.kernelParams = ["rcu_nocbs=0-15" "processor.max_cstate=5"]; # Ryzen lockup patches"
 
-  # TODO Put correct filesystems here
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-uuid/e31230ab-566c-478f-a078-d2db637696b6";
-  #   fsType = "ext4";
-  # };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/981b7c9a-6e90-4d2b-8693-dbed31ca8228";
+      fsType = "ext4";
+    };
 
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-uuid/042D-AF32";
-  #   fsType = "vfat";
-  # };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/B5C3-1655";
+      fsType = "vfat";
+    };
 
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 8 * 1024;
-      randomEncryption.enable = true;
-    }
-  ];
-
-  zramSwap = {
-    enable = true;
-    memoryPercent = 40;
-    priority = 10;
-  };
+  swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp12s0f0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp12s0f1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
