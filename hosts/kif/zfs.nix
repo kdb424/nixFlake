0d3e997a8ddaa56d19425c26ba41c3c3c5c107e5
Kyle Brown <kdb424@gmail.com>
2023-12-06 17:24:38 -0800

    feat: ZFS on kif

--- hosts/kif/zfs.nix
+++ hosts/kif/zfs.nix
@@ -10,7 +10,7 @@
     ../../common/nixos/zfs.nix
   ];
   networking.hostId = "4b0bddd5";
-  #boot.zfs.extraPools = [""];
+  boot.zfs.extraPools = ["inland"];
 
   services.zfs.autoSnapshot.enable = true;
 }
