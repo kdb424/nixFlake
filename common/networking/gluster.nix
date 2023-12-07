5a1c902d0233af2302f28ba30de4fec23ddaaac9
Kyle Brown <kdb424@gmail.com>
2023-12-06 17:24:02 -0800

    feat: Gluster mounts

--- common/networking/gluster.nix
+++ common/networking/gluster.nix
@@ -1,8 +1,45 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   services.glusterfs = {
     enable = true;
     # killMode = "process";
   };
+
+  systemd.mounts = [
+    {
+      type = "glusterfs";
+      what = "127.0.0.1:/docker";
+      where = "/mnt/gdocker";
+    }
+    {
+      type = "glusterfs";
+      what = "127.0.0.1:/media";
+      where = "/mnt/gmedia";
+    }
+    {
+      type = "glusterfs";
+      what = "127.0.0.1:/dockerdata";
+      where = "/mnt/gdockerdata";
+    }
+  ];
+
+  systemd.automounts = [
+    {
+      wantedBy = ["multi-user.target"];
+      where = "/mnt/gdocker";
+    }
+    {
+      wantedBy = ["multi-user.target"];
+      where = "/mnt/gmedia";
+    }
+    {
+      wantedBy = ["multi-user.target"];
+      where = "/mnt/gdockerdata";
+    }
+  ];
+
 }
