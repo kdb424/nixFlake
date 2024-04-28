922c608acc4f9dab99aea78eeaaadb508894a28b
Kyle Brown <kdb424@gmail.com>
2024-04-28 11:52:59 -0700

    feat: Move kif from hardware to VM

--- hosts/kif/default.nix
+++ hosts/kif/default.nix
@@ -4,14 +4,13 @@
   pkgs,
   inputs,
   outputs,
+  home-manager,
   ...
 }: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ../../common/nixos/baremetal.nix
-    # ./zfs.nix
-    ../../common/nixos/remoteBuild.nix
+    ../../common/nixos/common.nix
     ../../common/networking/zerotier.nix
     ../../common/editors/emacs.nix
     ../../common/nixos/docker.nix
@@ -19,11 +18,9 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/disk/by-id/ata-INTEL_SSDSC2KW256G8_BTLA733601VG256CGN";
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
-  boot.loader.grub.configurationLimit = 10;
-
-  networking.hostName = "kif"; # Define your hostname.
+  networking.hostName = "kif";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -31,11 +28,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Rips audio CD's
-  environment.systemPackages = [
-    pkgs.cdparanoia
-    pkgs.flac
-  ];
+  services.earlyoom.enable = true;
 
   # Latest stable kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
